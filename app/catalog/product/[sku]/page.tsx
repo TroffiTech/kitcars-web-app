c3f04682e46056ee355d24d489823fef8d3bbfbd
someTrickyCase <snowinboots18@gmail.com>
2025-06-02 23:07:47 +0400

    redicrect from undefined product page, uri decodeing for product sku in product page

--- app/catalog/product/[sku]/page.tsx
+++ app/catalog/product/[sku]/page.tsx
@@ -3,12 +3,13 @@ import Footer from "@/components/shared/footer/Footer";
 import Header from "@/components/shared/header/Header";
 
 import StaticDescription from "@/components/staticDescription/staticDescription";
+import { redirect } from "next/navigation";
 
 export default async function ProductPage({ params }: { params: Promise<{ sku: string }> }) {
     const { sku } = await params;
     const allProducts = await readAllProductsFile();
-    const productData = allProducts.filter((item) => item.sku === sku)[0];
-
+    const productData = allProducts.filter((item) => item.sku === decodeURI(sku))[0];
+    if (!productData) redirect("/not-found");
     return (
         <main>
             <section className='flex-box__column'>
