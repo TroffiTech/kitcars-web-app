91cac0407e41ebd3641a4f48029e280c58857744
someTrickyCase <snowinboots18@gmail.com>
2025-06-13 05:10:41 +0400

    just formatted a bit

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -2,11 +2,11 @@ import { update } from "@/scripts/updateContent";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-    try {
-        console.log(`scheduled task executed at ${new Date().toISOString()}`);
-        await update();
-        return NextResponse.json({ success: true });
-    } catch {
-        return NextResponse.json({ error: "Failed" }, { status: 500 });
-    }
+	try {
+		console.log(`scheduled task executed at ${new Date().toISOString()}`);
+		await update();
+		return NextResponse.json({ success: true });
+	} catch {
+		return NextResponse.json({ error: "Failed" }, { status: 500 });
+	}
 }
--- scripts/utils/loadAllProducts.js
+++ scripts/utils/loadAllProducts.js
@@ -1,38 +1,46 @@
-async function fetchProduct(url){
-    const res = await fetch(url, {
-                headers: {
-                    authorization: `Basic ${btoa(
-                        process.env.WC_KEY + ":" + process.env.WC_SECRET
-                    )}`,
-                    "content-type": "application/json",
-                },
-            }
-        )
-    
-    return await res.json()
+async function fetchProduct(url) {
+  const res = await fetch(url, {
+    headers: {
+      authorization: `Basic ${btoa(
+        process.env.WC_KEY + ":" + process.env.WC_SECRET
+      )}`,
+      "content-type": "application/json",
+    },
+  });
+
+  return await res.json();
 }
 
 export default async function loadAllProducts() {
-    console.log('fetching products...')
+  console.log("fetching products...");
 
-    const res = await fetch(`${process.env.DB_URL}/wp-json/wc/v3/products?per_page=10`, {
-        headers: {
-            authorization: `Basic ${btoa(process.env.WC_KEY + ":" + process.env.WC_SECRET)}`,
-            "content-type": "application/json",
-        },
-    });
-    const productsQuantity = Number(res.headers.get("x-wp-total"));
-    const pagesQuantity = Math.ceil(productsQuantity / 100);
+  const res = await fetch(
+    `${process.env.DB_URL}/wp-json/wc/v3/products?per_page=10`,
+    {
+      headers: {
+        authorization: `Basic ${btoa(
+          process.env.WC_KEY + ":" + process.env.WC_SECRET
+        )}`,
+        "content-type": "application/json",
+      },
+    }
+  );
+  const productsQuantity = Number(res.headers.get("x-wp-total"));
+  const pagesQuantity = Math.ceil(productsQuantity / 100);
 
-    const promises = []
+  const promises = [];
 
-    new Array(pagesQuantity).fill(null).map((_, index) => {
-        const curPage = index + 1
-        promises.push(fetchProduct(`${process.env.DB_URL}/wp-json/wc/v3/products?per_page=100&page=${curPage}`))
-    })
-    
-    const allProducts = await Promise.all(promises)
+  new Array(pagesQuantity).fill(null).map((_, index) => {
+    const curPage = index + 1;
+    promises.push(
+      fetchProduct(
+        `${process.env.DB_URL}/wp-json/wc/v3/products?per_page=100&page=${curPage}`
+      )
+    );
+  });
 
-    console.info("products list object are loaded");
-    return allProducts.flat();
+  const allProducts = await Promise.all(promises);
+
+  console.info("products list object are loaded");
+  return allProducts.flat();
 }
