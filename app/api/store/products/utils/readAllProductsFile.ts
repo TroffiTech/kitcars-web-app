b9082cb4e8795d61ea4af9b81c9ede733977157d
someTrickyCase <snowinboots18@gmail.com>
2025-06-20 05:30:02 +0400

    types annotation in readAAllProductsFile function changed to unknown, and type checking added

--- app/api/store/products/utils/readAllProductsFile.ts
+++ app/api/store/products/utils/readAllProductsFile.ts
@@ -1,15 +1,17 @@
-import { Product } from "@/types/productsType";
 import loadAllProducts from "../../../../../scripts/utils/loadAllProducts";
 import writeAllProductsFile from "../../../../../scripts/utils/writeAllProductsFile";
 import allProducts from "@/app/content/allProducts.json";
 
 export async function readAllProductsFile() {
-    if (allProducts)
-        return (allProducts as Array<Product>).filter((item) => item.status === "publish");
-    else {
-        const allProductsLoaded = await loadAllProducts();
-        writeAllProductsFile(allProductsLoaded);
+	if (allProducts)
+		return (allProducts as Array<unknown>).filter(
+			(item) =>
+				item instanceof Object && "status" in item && item.status === "publish"
+		);
+	else {
+		const allProductsLoaded = await loadAllProducts();
+		writeAllProductsFile(allProductsLoaded);
 
-        return allProductsLoaded.filter((item) => item.status === "publish");
-    }
+		return allProductsLoaded.filter((item) => item.status === "publish");
+	}
 }
